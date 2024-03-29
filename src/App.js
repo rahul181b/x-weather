89033bf21405fe9496094f9d70469b3ed775bc04
Rahul <bhaleraor17@gmail.com>
2024-03-29 13:36:14 +0530

    x-weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = async (city) => {
+    try {
+      if (city) {
+        setIsLoading(true);
+        const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=2b183e66c581405c8cb113315230912&q=${city}&aqi=no`);
+        setData(res.data);
+        setIsLoading(false);
+      }
+      else {
+        return;
+      }
+    } catch (e) {
+      alert("Failed to fetch weather-card data");
+      console.log(e);
+      setData(null);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (city) {
+      setData(null)
+    }
+  }, [city]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="display-search">
+        <input className="searchbox" type="text" placeholder="Enter city name" onChange={(e) => setCity(e.target.value)} />
+        <button type="submit" onClick={() => handleSearch(city)}>Search</button>
+      </div>
+      {isLoading && <div style={{ textAlign: "center", margin: "auto" }}> <p>Loading...</p> </div>}
+      {data != null ? (
+        <div className="display-Data">
+          <div className="weather-card">
+            <h2>Temperature</h2>
+            <p>{data.current.temp_c}°C</p>
+          </div>
+          <div className="weather-card">
+            <h2>Humidity</h2>
+            <p>{data.current.humidity}%</p>
+          </div>
+          <div className="weather-card">
+            <h2>Condition</h2>
+            <p>{data.current.condition.text}</p>
+          </div>
+          <div className="weather-card">
+            <h2>Wind speed</h2>
+            <p>{data.current.wind_kph} km/h</p>
+          </div>
+        </div>
+      ) : <div></div>}
     </div>
   );
 }
