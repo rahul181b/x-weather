3aace5143440cf6d5fc20c151b93b117efe3eaf0
Rahul <bhaleraor17@gmail.com>
2024-03-29 14:02:28 +0530

    changed the classes name

--- src/App.js
+++ src/App.js
@@ -40,20 +40,20 @@ function App() {
       </div>
       {isLoading && <div style={{ textAlign: "center", margin: "auto" }}> <p>Loading data...</p> </div>}
       {data != null ? (
-        <div className="display-Data">
-          <div className="weather-cards">
+        <div className="weather-cards">
+          <div className="weather-card">
             <h2>Temperature</h2>
             <p>{data.current.temp_c}°C</p>
           </div>
-          <div className="weather-cards">
+          <div className="weather-card">
             <h2>Humidity</h2>
             <p>{data.current.humidity}%</p>
           </div>
-          <div className="weather-cards">
+          <div className="weather-card">
             <h2>Condition</h2>
             <p>{data.current.condition.text}</p>
           </div>
-          <div className="weather-cards">
+          <div className="weather-card">
             <h2>Wind speed</h2>
             <p>{data.current.wind_kph} km/h</p>
           </div>
